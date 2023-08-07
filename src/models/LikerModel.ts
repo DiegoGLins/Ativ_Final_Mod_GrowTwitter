d393e960d2dbdab3717577cb9115d46842832b49
Diego G.Lins <dl7designer@gmail.com>
2023-08-07 09:42:32 -0300

    atualização

--- src/models/LikerModel.ts
+++ src/models/LikerModel.ts
@@ -1,18 +1,17 @@
 import { v4 as createUuid } from 'uuid'
 import { users } from "../database/users";
-import Tweet from './TweetModel';
-import { Reply } from './ReplyModel';
-import { User } from './UserModel';
 import { Follow } from '../Types/FollowUserType';
 
 export class Liker {
     public likedId: string;
     public authorlike: Follow
     public tweetLiked: string
+    public like: string
     constructor(tweetLiked: string, authorlike: Follow) {
         this.tweetLiked = tweetLiked;
         this.likedId = createUuid();
         this.authorlike = authorlike
+        this.like = '\uD83D\uDC4D'
     }
 
 
@@ -21,4 +20,6 @@ export class Liker {
 
         return { name: name?.detailFollow().username}
     }
+
+    
 }
