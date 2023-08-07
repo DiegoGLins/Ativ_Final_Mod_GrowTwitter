05be6eb0a2624debea472f74e0b4217f71fce03b
Diego G.Lins <dl7designer@gmail.com>
2023-08-06 21:07:24 -0300

    refatoração

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 
-import { TweetController } from "./controllers/TweetController";
+import LikerController from "./controllers/LikerController";
+import ReplyController from "./controllers/ReplyController";
+import TweetController from "./controllers/TweetController";
 import UserController from "./controllers/UserController";
+import { Reply } from "./models/ReplyModel";
 import Tweet from "./models/TweetModel";
 import { User } from "./models/UserModel";
 
@@ -23,25 +26,120 @@ UserController.registerUser(user3);
 console.log(user3.detailUserRegister())
 console.log("==================================================================================")
 
+console.log("==================================================================================")
+
+console.log("=============== Instanciando um tweet para o usuario Leila(user2) ================")
+const tweetA = new Tweet("A Growdev é show :)", user2)
+console.log("==================================================================================")
+
+console.log("========== Criando o tweet do usuario Leila(user2) com o TweetController =========")
+const createTweetA = TweetController.create(tweetA, user2)
+console.log(createTweetA)
+
+console.log("==================================================================================")
+console.log(`Usuario ${tweetA.author.username}`)
+console.log("==================================================================================")
+console.log(user2)
+
+console.log("==================================================================================")
+
+console.log("============== Instanciando um tweet para o usuario Alan(user3) ==================")
+const tweetB = new Tweet("Bora Growdevers", user3)
+console.log("==================================================================================")
+
+console.log("=========== Criando o tweet do usuario Alan(user3) com o TweetController =========")
+const createTweetB = TweetController.create(tweetB, user3)
+console.log(createTweetB)
+
+console.log("==================================================================================")
+console.log(`Usuario ${tweetB.author.username}`)
+console.log("==================================================================================")
+console.log(user3)
+
+console.log("==================================================================================")
+
+console.log("================ Instanciando um tweet para o usuario Jonh(user1) ================")
+const tweetC = new Tweet("Aprendi nessa plataforma com a Growdev", user1)
+console.log("==================================================================================")
+
+console.log("=========== Criando o tweet do usuario Jonh(user1) com o TweetController =========")
+const createTweetC = TweetController.create(tweetC, user1)
+console.log(createTweetC)
+
+console.log("==================================================================================")
+console.log(`Usuario ${tweetC.author.username}`)
+console.log("==================================================================================")
+console.log(user1)
+
+console.log("==================================================================================")
+
+console.log("= Instanciando um tweet reply do usuario Alan(user3) para o tweet de Jonh(user1) =")
+const tweetD = new Reply("Aprendi nessa plataforma com a Growdev", user1, user3, "Evolui muito com a Growdev")
+console.log("==================================================================================")
+console.log("====== Criando o tweet reply do usuario Alan(user3) com o ReplyController ========")
+
+const createReplyD = ReplyController.createReply(user1, "Aprendi nessa plataforma com a Growdev",tweetD)
+console.log(createReplyD)
+
+console.log("= Instanciando um tweet reply do usuario Jonh(user1) para o tweet de Leila(user2) =")
+const tweetE = new Reply("A Growdev é show :)", user2, user1, "Gosto do sistema de ensino da Growdev")
+console.log("===================================================================================")
+console.log("====== Criando o tweet reply do usuario Jonh(user1) com o ReplyController ========")
+
+const createReplyE = ReplyController.createReply(user2, "A Growdev é show :)",tweetE)
+console.log(createReplyE)
+
+console.log("= Instanciando um tweet reply do usuario Alan(user3) para o tweet de Leila(user2) =")
+const tweetF = new Reply("A Growdev é show :)", user2, user3, "Vamos decolar com a Growdev")
+console.log("===================================================================================")
+console.log("====== Criando o tweet reply do usuario Alan(user3) com o ReplyController ========")
+
+const createReplyF = ReplyController.createReply(user2, "A Growdev é show :)",tweetF)
+console.log(createReplyF)
+
+console.log("=================== Usuario Jonh curte o tweet de usuario Leila ==================")
+
+const liked1 = LikerController.addLike(user1, "A Growdev é show :)", user2)
+console.log(liked1)
+
+console.log("=================== Usuario Jonh curte o tweet de usuario Alan ===================")
+
+const liked2 = LikerController.addLike(user1, "Evolui muito com a Growdev", user3)
+console.log(liked2)
+console.log("==================================================================================")
+
+console.log("================== Usuario Alan curte o tweet de usuario Leila ===================")
+
+const liked3 = LikerController.addLike(user3, "A Growdev é show :)", user2)
+console.log(liked3)
+console.log("==================================================================================")
+
+console.log("================== Usuario Leila curte o tweet de usuario Jonh ===================")
+
+const liked4 = LikerController.addLike(user2, "Aprendi nessa plataforma com a Growdev", user1)
+console.log(liked4)
+console.log("==================================================================================")
+
+
 console.log(`Usuario ${user1.name}`)
 
 console.log("============ Usuario Jonh(user1) adicionando e removendo seguidores ==============")
 
-user1.followingUser(user1.detailFollow()); //usuario Jonh tenta seguir a si mesmo
-user1.followingUser(user2.detailFollow()); // usuario Jonh seguindo usuario Leila
-user1.followingUser(user3.detailFollow()); // usuario Jonh seguindo usuario Alan
-user1.unFollowingUser(user2.detailFollow()); //usuario Jonh deixando de seguir usuario Leila
+user1.followingUser(user1); //usuario Jonh tenta seguir a si mesmo
+user1.followingUser(user2); // usuario Jonh seguindo usuario Leila
+user1.followingUser(user3); // usuario Jonh seguindo usuario Alan
+user1.unFollowingUser(user2); //usuario Jonh deixando de seguir usuario Leila
 console.log(user1.detailFollow())
 
 console.log("==================================================================================")
 
 console.log(`Usuario ${user2.name}`)
 
-console.log("============= Usuario Leila(user2) adicionando e removendo seguidores ============")
+console.log("============ Usuario Leila(user2) adicionando e removendo seguidores =============")
 
-user2.followingUser(user3.detailFollow()); // usuario Leila seguindo usuario Alan
-user2.followingUser(user1.detailFollow()); // usuario Leila seguindo usuario jonh
-user2.followingUser(user2.detailFollow()); // usuario Leila tenta seguir a si mesmo
+user2.followingUser(user3); // usuario Leila seguindo usuario Alan
+user2.followingUser(user1); // usuario Leila seguindo usuario jonh
+user2.followingUser(user2); // usuario Leila tenta seguir a si mesmo
 console.log(user2.detailFollow())
 
 console.log("==================================================================================")
@@ -50,109 +148,26 @@ console.log(`Usuario ${user3.name}`)
 
 console.log("============= Usuario Alan(user3) adicionando e removendo seguidores =============")
 
-user3.followingUser(user1.detailFollow()); // usuario Alan seguindo usuario Jonh
-user3.followingUser(user1.detailFollow()); // usuario Alan seguindo usuario Jonh
-user3.followingUser(user2.detailFollow()); // usuario Alan seguindo usuario Leila
-user3.unFollowingUser(user2.detailFollow()); //usuario Alan deixou de seguir usuario Leila
-user3.unFollowingUser(user2.detailFollow()); //usuario Alan deixou de seguir usuario Leila novamente e retorna alerta
+user3.followingUser(user1); // usuario Alan seguindo usuario Jonh
+user3.followingUser(user1); // usuario Alan seguindo usuario Jonh
+user3.followingUser(user2); // usuario Alan seguindo usuario Leila
+user3.unFollowingUser(user2); //usuario Alan deixou de seguir usuario Leila
+user3.unFollowingUser(user2); //usuario Alan deixou de seguir usuario Leila novamente e retorna alerta
 console.log(user3.detailFollow())
 
-console.log("==================================================================================")
-
-console.log("=============== Instanciando um tweet para o usuario Leila(user2) ================")
-const tweetA = new Tweet("A Growdev é show :)", user2)
-console.log("==================================================================================")
-
-console.log("========== Criando o tweet do usuario Leila(user2) com o TweetController =========")
-new TweetController().create(tweetA)
 
 
-console.log("==================================================================================")
-console.log(`Usuario ${tweetA.author?.name}`)
-console.log("==================================================================================")
-console.log(user2.detailFollow())
+console.log(`===================== Feed do usuario ${user2.name} ==============================`)
+TweetController.feedTweet("A Growdev é show :)", user2)
 
-console.log("==================================================================================")
+console.log("-------------------------------------------------------------------------")
 
-console.log("= Instanciando um tweet reply do usuario Jonh(user1) para o tweet de Leila(user2)=")
-const tweetC = new Tweet("Também gosto da Growdev", user1,tweetA)
-console.log("==================================================================================")
+console.log(`===================== Feed do usuario ${user3.name} ==============================`)
+TweetController.feedTweet("Bora Growdevers", user3)
+TweetController.feedTweet("Evolui muito com a Growdev", user3)
 
-console.log("======= Criando o tweet reply do usuario Jonh(user1) com o TweetController =======")
-new TweetController().create(tweetC)
+console.log("-------------------------------------------------------------------------")
 
 
-console.log("==================================================================================")
-console.log(`Usuario ${tweetC.author?.name}`)
-console.log("==================================================================================")
-console.log(user1.detailFollow())
-
-console.log("==================================================================================")
-
-console.log("======== Instanciando um tweet para o usuario Alan(user3) ========================")
-const tweetB = new Tweet("Bora Growdevers", user3)
-console.log("==================================================================================")
-
-console.log("= Instanciando um tweet reply do usuario Alan(user3) para o tweet de Jonh(user1) =")
-const tweetD = new Tweet("Consegui evoluir bastante com a Growdev", user3,tweetC)
-console.log("==================================================================================")
-
-console.log("========== Criando o tweet do usuario Alan(user3) com o TweetController ==========")
-new TweetController().create(tweetB)
-console.log("==================================================================================")
-
-console.log("=== Criando o tweetD reply do usuario Alan(user3) para o tweetC de Jonh(user1) ===")
-new TweetController().create(tweetD)
-
-
-console.log("==================================================================================")
-console.log(`Usuario ${tweetB.author?.name}`)
-console.log("==================================================================================")
-console.log(user3.detailFollow())
-
-console.log("==================================================================================")
-
-console.log("========== Usuario Jonh(user1) curtindo tweetD de usuario Alan(user3) ============")
-// // const liked1 = tweetA.addTweetliked(tweetA)
-// console.log(liked1)
-
-
-console.log("=========================== Usuario Alan(user3) ==================================")
-console.log(user3.detailFollow())
-
-console.log("=========================== Usuario Jonh(user1) ==================================")
-console.log(user1.detailFollow())
-
-// const liked
-// console.log(liked)
-
-
-
-// const list = new TweetController().listAllTweets()
-// console.log(list)
-
-
-// // const result = tweet1.displayTweet()
-// // // console.log(user2.detailFollow())
-// // console.log(result)
-
-
-
-// // console.log(`Usuario ${tweet2.author.name}`)
-// // new TweetController().create(tweet2)
-// // console.log(user1.detailFollow())
-
-
-
-// // const detailUser = user1.detailFollow()
-
-// // const allTweets = detailUser.tweets.map(item=> {
-// //    const info = item.displayTweet()
-// //    return {info, autor: item.author}
-// // })
-// // const parse = JSON.stringify(allTweets)
-// // console.log(`id: ${detailUser.id}`)
-// // console.log(`name: ${detailUser.name}`)
-// // console.log(`username: ${detailUser.username}`)
-// // console.log(`tweets: ${JSON.parse(parse)}`)
-// // console.log(detailUser, allTweets)
+console.log(`==================== Feed do usuario ${user1.name} ===============================`)
+TweetController.feedTweet("Aprendi nessa plataforma com a Growdev", user1)
