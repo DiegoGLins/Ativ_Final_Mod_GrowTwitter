f86239fc32abc8578f57146266ecfb8aacfb38d8
Diego G.Lins <dl7designer@gmail.com>
2023-08-07 10:51:36 -0300

    atualização

--- src/models/LikerModel.ts
+++ src/models/LikerModel.ts
@@ -6,12 +6,10 @@ export class Liker {
     public likedId: string;
     public authorlike: Follow
     public tweetLiked: string
-    public like: string
     constructor(tweetLiked: string, authorlike: Follow) {
         this.tweetLiked = tweetLiked;
         this.likedId = createUuid();
         this.authorlike = authorlike
-        this.like = '\uD83D\uDC4D'
     }
 
 }
