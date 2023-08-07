a06c9cb0672d703dcc34c7a63d4673e562f6fbfb
Diego G.Lins <dl7designer@gmail.com>
2023-08-06 21:11:47 -0300

    classe LikerModel

--- src/models/LikerModel.ts
+++ src/models/LikerModel.ts
@@ -1,12 +1,24 @@
-import { Follow } from "../Types/FollowUserType";
-import Tweet from "./TweetModel";
-
+import { v4 as createUuid } from 'uuid'
+import { users } from "../database/users";
+import Tweet from './TweetModel';
+import { Reply } from './ReplyModel';
+import { User } from './UserModel';
+import { Follow } from '../Types/FollowUserType';
 
 export class Liker {
-    public tweetLiked: Tweet;
+    public likedId: string;
     public authorlike: Follow
-    constructor(tweetLiked: Tweet, authorlike: Follow) {
+    public tweetLiked: string
+    constructor(tweetLiked: string, authorlike: Follow) {
         this.tweetLiked = tweetLiked;
+        this.likedId = createUuid();
         this.authorlike = authorlike
+    }
+
+
+    public nameAuthorLike() {
+        const name = users.find(item => item.detailFollow() === this.authorlike)
+
+        return { name: name?.detailFollow().username}
     }
 }
