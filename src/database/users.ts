6aa73e9c897ac25d33b07ca5bf6deaa8fdc3078f
Diego G.Lins <dl7designer@gmail.com>
2023-08-06 21:10:51 -0300

    database users

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,6 +1,3 @@
 import { User } from "../models/UserModel";
 
-export const users: User[] = [
-    new User("Diego","lins7","diego@diego","12345"),
-    new User("Luana","lua6987","luana@luana","12345"),
-]
+export const users: User[] = []
